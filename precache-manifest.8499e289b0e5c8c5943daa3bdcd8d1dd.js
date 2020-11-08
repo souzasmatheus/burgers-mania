5bfd3f93f0a3f69cefba53b530d4d98f017262b0
Matheus Souza <matheu.sk.9@hotmail.com>
2020-11-07 22:33:03 -0300

    Updates

---
+++ precache-manifest.8499e289b0e5c8c5943daa3bdcd8d1dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb948c162ace3fd840cc7416ca87bafd",
+    "url": "/burgers-mania/index.html"
+  },
+  {
+    "revision": "2e2f0a87934af4e1bb2c",
+    "url": "/burgers-mania/static/css/main.d4dde59c.chunk.css"
+  },
+  {
+    "revision": "59cd7d99297af26cc3a2",
+    "url": "/burgers-mania/static/js/2.4d9e1904.chunk.js"
+  },
+  {
+    "revision": "3ae8240266c7f67b942dab69ea230db8",
+    "url": "/burgers-mania/static/js/2.4d9e1904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2f0a87934af4e1bb2c",
+    "url": "/burgers-mania/static/js/main.3f8d2c6c.chunk.js"
+  },
+  {
+    "revision": "200e2bd9a1dc99060a54",
+    "url": "/burgers-mania/static/js/runtime-main.05bdbbb2.js"
+  },
+  {
+    "revision": "494a3f582ee290ea7272728a9bfb5d03",
+    "url": "/burgers-mania/static/media/bauru.494a3f58.jpg"
+  },
+  {
+    "revision": "f1bb13023bb71b9d791863ca20422046",
+    "url": "/burgers-mania/static/media/burger.f1bb1302.jpg"
+  },
+  {
+    "revision": "1b264e5abcbbc073768e5ff41afab99d",
+    "url": "/burgers-mania/static/media/fries.1b264e5a.jpg"
+  },
+  {
+    "revision": "fa4c1047192db2e4364ab977eb49dc70",
+    "url": "/burgers-mania/static/media/logo.fa4c1047.png"
+  }
+]);
