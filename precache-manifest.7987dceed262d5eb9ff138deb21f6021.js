c9f0eea12a57e3a0967364023e6d4316bcd25260
Matheus Souza <matheu.sk.9@hotmail.com>
2020-12-11 18:06:31 -0300

    Updates

---
+++ precache-manifest.7987dceed262d5eb9ff138deb21f6021.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0987fb7eefafb9006aadaf6553add2",
+    "url": "/www.burguers-mania.com.br/index.html"
+  },
+  {
+    "revision": "d3062d1f29674dc04bda",
+    "url": "/www.burguers-mania.com.br/static/css/main.d4dde59c.chunk.css"
+  },
+  {
+    "revision": "b461a1f21fe8a417a042",
+    "url": "/www.burguers-mania.com.br/static/js/2.d0bd7b56.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/www.burguers-mania.com.br/static/js/2.d0bd7b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3062d1f29674dc04bda",
+    "url": "/www.burguers-mania.com.br/static/js/main.d6911725.chunk.js"
+  },
+  {
+    "revision": "0879068922bde179141e",
+    "url": "/www.burguers-mania.com.br/static/js/runtime-main.d6c74169.js"
+  },
+  {
+    "revision": "0fd07923c19c1f8f56e94cf69fa4be89",
+    "url": "/www.burguers-mania.com.br/static/media/default-batatas.0fd07923.jpeg"
+  },
+  {
+    "revision": "f210f953f33f83eae09d098dd992befd",
+    "url": "/www.burguers-mania.com.br/static/media/default-baurus.f210f953.jpeg"
+  },
+  {
+    "revision": "986d553e16acd2de94e5110b41c626e4",
+    "url": "/www.burguers-mania.com.br/static/media/default-burgers.986d553e.jpeg"
+  },
+  {
+    "revision": "fa4c1047192db2e4364ab977eb49dc70",
+    "url": "/www.burguers-mania.com.br/static/media/logo.fa4c1047.png"
+  }
+]);
